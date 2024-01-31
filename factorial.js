1185067f428b07c40eadef205b9389f54e426b86
tylerReis <tyler.reis.j@outlook.com>
2024-01-31 11:57:23 -0500

    for loop method

--- factorial.js
+++ factorial.js
@@ -1,8 +1,9 @@
 function factorial(num) {
-  if (num <= 0){
-    return 1
+  let answer = 1
+  for (let i = num; i > 0; i--){
+    answer *= i
   }
-  return num*factorial(num-1)
+  return answer
 }
 
 module.exports = factorial;
